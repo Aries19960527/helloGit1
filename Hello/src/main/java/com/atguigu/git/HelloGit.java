19fd2b1ecb82fd08dfeea1720ee08d1031a657a0
aries <757930547@qq.com>
2020-10-27 15:54:36 +0800

    test

---
+++ Hello/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * Author：  aries
+ * Date： 2020/10/27
+ * Desc ：
+ */
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
